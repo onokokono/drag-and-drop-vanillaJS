cea3fdfa008adb799bc6964cc36ba87a6c333c8a
Issac <onokokono@gmail.com>
2020-05-12 22:26:33 -0700

    little change

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 window.addEventListener('load', () => {
 
-    const draggables = document.querySelectorAll('.draggable');
+    const cards = document.querySelectorAll('.draggable');
     const containers = document.querySelectorAll('.container');
 
-    draggables.forEach(element => {
-        element.addEventListener('dragstart', () => {
-            element.classList.add('dragging');
+    cards.forEach(card => {
+        card.addEventListener('dragstart', () => {
+            card.classList.add('dragging');
         });
-        element.addEventListener('dragend', () => {
-            element.classList.remove('dragging');
+        card.addEventListener('dragend', () => {
+            card.classList.remove('dragging');
         });
     });
 
     containers.forEach(container => {
         container.addEventListener('dragover', (event) => {
-            const card = document.querySelector('.dragging');
+            const draggingCard = document.querySelector('.dragging');
             const afterElement = getAfterElement(container, event.clientY);
-            if(!afterElement)
-                container.appendChild(card);
-            else
-                container.insertBefore(card, afterElement);
+            if (!afterElement) container.appendChild(draggingCard);
+            else container.insertBefore(draggingCard, afterElement);
         });
     });
 
     function getAfterElement(container, y) {
-        const draggables = [...container.querySelectorAll('.draggable:not(.dragging)')];
+        const cards = [...container.querySelectorAll('.draggable:not(.dragging)')];
 
-        return draggables.reduce((closest, child) => {
+        return cards.reduce((closest, child) => {
             const box = child.getBoundingClientRect();
-            const offset = y - box.top - box.height/2;
-            if(offset < 0 && offset > closest.offset)
-                return {offset: offset, element: child};
-            else return closest;
+            const offset = y - box.top - box.height / 2;
+            if (offset < 0 && offset > closest.offset)
+                return { offset: offset, element: child }
+            else
+                return closest;
 
         }, { offset: Number.NEGATIVE_INFINITY }).element;
-    };
+    }
 
 });
-
